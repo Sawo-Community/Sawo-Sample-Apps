54f2cdacaf328206289b7b47b2fea51290f0d96f
SawoCommunity <91311893+SawoCommunity@users.noreply.github.com>
2021-10-14 18:45:07 +0530

    Create Login.js

---
+++ React-Native-Sample-App/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text>This is Login page</Text>
+    </View>
+  );
+};
+
+export { Login };
