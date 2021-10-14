414feb95f718801dcc2cbc55049b7908db697d2d
SawoCommunity <91311893+SawoCommunity@users.noreply.github.com>
2021-10-14 19:58:16 +0530

    Create FrameworkV1.h

---
+++ ios-framework/FrameworkV1/FrameworkV1/FrameworkV1.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkV1.h
+//  FrameworkV1
+//
+//  Created by Rhytthm Mahajan on 10/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkV1.
+FOUNDATION_EXPORT double FrameworkV1VersionNumber;
+
+//! Project version string for FrameworkV1.
+FOUNDATION_EXPORT const unsigned char FrameworkV1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkV1/PublicHeader.h>
+
+
