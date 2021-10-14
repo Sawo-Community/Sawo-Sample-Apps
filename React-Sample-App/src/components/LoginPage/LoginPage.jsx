34a29a83556e4515370330167419c43e26e78b36
SawoCommunity <91311893+SawoCommunity@users.noreply.github.com>
2021-10-14 17:35:58 +0530

    Create LoginPage.jsx

---
+++ React-Sample-App/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import Sawo from "sawo";
+
+import "./LoginPage.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const LoginPage = () => {
+  const [isUserLoggedIn, setUserLoggedIn] = useState(false);
+  const [payload, setPayload] = useState({});
+
+  useEffect(() => {
+    var config = {
+      containerID: "sawo-container",
+      identifierType: "email",
+      apiKey: API_KEY,
+      onSuccess: (payload) => {
+        console.log("Payload : " + JSON.stringify(payload));
+        setUserLoggedIn(true);
+        setPayload(payload);
+      },
+    };
+    let sawo = new Sawo(config);
+    sawo.showForm();
+  }, []);
+
+  return (
+    <div className="containerStyle">
+      <section>
+        <h2 className="title">SAWO React Example App</h2>
+        <h2 className="title">User Logged In : {isUserLoggedIn.toString()}</h2>
+
+        {!isUserLoggedIn ? (
+          <div className="formContainer" id="sawo-container"></div>
+        ) : (
+          <div className="loggedin">
+            <h2>User Successful Login</h2>
+            <div>UserId: {payload.user_id}</div>
+            <div>Verification Token: {payload.verification_token}</div>
+          </div>
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default LoginPage;
